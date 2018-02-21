e4bc5b33799a84b652d3b57f8b947a4ffcf9b825
shossain97 <shafeen.hossain97@gmail.com>
2018-02-21 14:50:02 -0600

    creating job queue, job struct, process queue, process struct

---
+++ quash/src/processQueue.h
@@ -0,0 +1,12 @@
+#ifndef PROCESSQUEUE_H
+#define PROCESSQUEUE_H
+
+#include "deque.h"
+#include "command.h"
+#include <stdio.h>
+#include <unistd.h>
+
+typedef struct process{
+	char* cmd;
+	pid_t pid;
+}
